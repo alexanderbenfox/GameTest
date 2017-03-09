06fe4bbce369adb72022ca7e808ed87ef22f570e
Alex Fox <alexanderbenfox@gmail.com>
2017-03-09 18:41:31 -0500

    made it easier to use...?

---
+++ install-frameworks.sh
@@ -0,0 +1,5 @@
+mv frameworks/SDL.framework ~/Library/Frameworks/
+mv frameworks/SDL2.framework ~/Library/Frameworks/
+mv frameworks/SDL2_image.framework ~/Library/Frameworks/
+mv frameworks/SDL2_mixer.framework ~/Library/Frameworks/
+mv frameworks/SDL2_ttf.framework ~/Library/Frameworks/
